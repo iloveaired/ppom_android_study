d76b8a2a48b34c04f1ad38ad0989817ea9723114
iloveaired <iloveaired@gmail.com>
2023-01-06 00:35:12 +0900

    RecyclerView 기본구현

--- app/src/main/java/com/ppomppu/android/adapters/MyAdapter.java
+++ app/src/main/java/com/ppomppu/android/adapters/MyAdapter.java
@@ -1,37 +1,42 @@
 package com.ppomppu.android.adapters;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.ppomppu.android.R;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
+    private static final String TAG = "로그";
     private String[] mDataset;
 
     public MyAdapter(String[] myDataset) {
+
         mDataset = myDataset;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView mTextView;
+        private final TextView textView;
+
         public ViewHolder(View v) {
             super(v);
 
+            this.textView = v.findViewById(R.id.textView);
+        }
+        public TextView getTextView() {
+            return textView;
         }
     }
 
     @Override
     public  ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view, parent, false);
-
-        //findViewById...
-
+        Log.d(TAG, "MyAdapter::onCreateViewHolder: ");
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.text_row_item, parent, false);
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
@@ -39,7 +44,10 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-      //  holder.mTextView.setText(mDataset[position]);
+        // Get element from your dataset at this position and replace the contents of the view
+        // with that element
+        holder.getTextView().setText(mDataset[position]);
+        Log.d(TAG, "Element " + mDataset[position]);
     }
 
     @Override
