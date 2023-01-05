dcee73c1ce32decd1153fff631ef22d1721afa2a
iloveaired <iloveaired@gmail.com>
2023-01-05 16:59:22 +0900

    로그추가

--- app/src/main/java/com/ppomppu/android/MainActivity.java
+++ app/src/main/java/com/ppomppu/android/MainActivity.java
@@ -3,11 +3,14 @@ package com.ppomppu.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "로그";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "MainActivity::onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
