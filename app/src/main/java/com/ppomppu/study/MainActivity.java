cd127e2ae224a04027650cfd6741c1a49d2fd605
iloveaired <iloveaired@gmail.com>
2023-01-07 20:45:30 +0900

    뷰모델 전단계 activity 생명주기 파악

--- app/src/main/java/com/ppomppu/study/MainActivity.java
+++ app/src/main/java/com/ppomppu/study/MainActivity.java
@@ -4,13 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ppomppu.study.databinding.ActivityCounterBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private  int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        ActivityCounterBinding binding = ActivityCounterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.textviewCount.setText("0");
+
+        binding.fabAdd.setOnClickListener( v -> {
+            counter++;
+            binding.textviewCount.setText(counter+"");
+        });
+
+        binding.fabRemove.setOnClickListener( v -> {
+            counter--;
+            binding.textviewCount.setText(counter+"");
+        });
 
     }
 }
